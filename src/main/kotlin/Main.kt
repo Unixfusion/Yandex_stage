2cc0d05a96e9278de2d557442210e4bc2b21fe88
UnixF <unixfusion@gmail.com>
2022-06-13 19:37:33 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+fun main(args: Array<String>) {
+    var count = readLine()!!.toInt()
+    println(count)
+    var set = readLine()!!.split(" ").map { it.toInt() }
+    var negative = mutableListOf<Int>()
+    var positive = mutableListOf<Int>()
+    var result = 0
+    for (item in set) {
+        if (item < 0)  {
+            negative.add(item)
+        } else {
+            positive.add(item)
+        }
+    }
+
+    result = when {
+        negative.isEmpty() -> positive.minOrNull()!!
+        positive.isEmpty() -> if (negative.size%2 == 0) {
+                                negative.minOrNull()!!
+                              } else { negative.maxOrNull()!! }
+        negative.size%2 == 0 -> positive.minOrNull()!!
+        else -> negative.maxOrNull()!!
+    }
+    println(result)
+}
